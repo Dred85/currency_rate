c49bd1e0f70d36e5b202051df7bb8a5b37853eb3
Olonov Aleksandr <adrolv08111985@gmail.com>
2024-06-08 16:47:39 +0300

    add utils.py

--- main.py
+++ main.py
@@ -1,41 +1,7 @@
-import json
-import os
+
 from datetime import datetime
 
-from environs import Env
-
-import requests
-
-
-env = Env()  # Создаем экземпляр класса Env
-env.read_env()  # Методом read_env() читаем файл .env и загружаем из него переменные в окружение
-
-CURRENCY_RATES_FILE = "currency_rates.json"
-API_KEY = env('EXCHANGE_RATE_API_KEY')
-
-
-def get_currency_rate(currency: str) -> float:
-    """Получает курс валюты от API и возвращает его в виде float"""
-
-    url = f"https://api.apilayer.com/exchangerates_data/latest?base={currency}"
-    response = requests.get(url, headers={'apikey': API_KEY})
-    response_data = json.loads(response.text)
-    rate = response_data["rates"]["RUB"]
-    return rate
-
-
-def save_to_json(data: dict) -> None:
-    """Сохраняет данные в json файл"""
-
-    with open(CURRENCY_RATES_FILE, "a") as f:
-        if os.stat(CURRENCY_RATES_FILE).st_size == 0:
-            json.dump([data], f)
-        else:
-            with open(CURRENCY_RATES_FILE) as json_file:
-                data_list = json.load(json_file)
-            data_list.append(data)
-            with open(CURRENCY_RATES_FILE, "w") as json_file:
-                json.dump(data_list, json_file)
+from utils import get_currency_rate, save_to_json
 
 
 def main():
